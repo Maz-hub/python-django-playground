c7d7a92168711309f166a0ccfd653317573d2e49
Maz <77918037+Maz-hub@users.noreply.github.com>
2024-12-09 15:13:42 +0000

    Simple loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+# simple loop, to count a bunch of numbers
+for i in [0, 1, 2, 3, 4, 5]: # python list
+    print(i)
+
+# ---
+
+for i in range(6):  # instead of writing [0, 1, 2, 3, 4, 5], use a built-in function range()
+    print(i)
+
+# ---    
+
+fruits = ['banana', 'apple', 'tomato']
+
+for fruit in fruits:
+    print(fruit)
+
+# ---    
+
+name = "Nathalie"
+
+for character in name:
+    print(character)
